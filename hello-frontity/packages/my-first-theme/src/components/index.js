559dc8a34a9e7f5bc5100b7eec0961dade54079b
Dave Mackey <dave@davemackey.net>
2021-03-31 13:46:12 -0400

    Update component

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -1,13 +1,23 @@
 // File: /packages/my-first-theme/src/components/index.js
 
 import React from "react"
+import { connect } from "frontity"
+import Link from "@frontity/components/link"
 
-const Root = () => {
+const Root = ({ state }) => {
     return (
       <>
-        <h1>Hello Frontity</h1>
+        <h1>Frontity Workshop</h1>
+        <p>Current URL: {state.router.link}</p>
+        <nav>
+          <Link link="/">Home</Link>
+          <br />
+          <Link link="/page/2">More Posts</Link>
+          <br />
+          <Link link="/about-us">Aboout Us</Link>
+        </nav>
       </>
     );
 };
 
-export default Root
+export default connect(Root)
